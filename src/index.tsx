12fc65ff5a9a6fe897d76c849ff781f6eb538229
Zuray <intheblackworld@gmail.com>
2018-08-13 18:17:28 +0800

    Add Redux devtool

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import Hello from './containers/hello'
 import { combineReducers } from 'redux-immutable' // pass immutable state, but happend the interface with immutable Obj problem
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 
 import userReducer from './store/reducers/user'
@@ -10,7 +11,10 @@ import userReducer from './store/reducers/user'
 const store = createStore(
   combineReducers({
     user: userReducer
-  })
+  }),
+  composeWithDevTools(
+    applyMiddleware(...[])
+  )
 )
 
 render(
